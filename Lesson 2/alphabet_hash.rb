ea6ff3abdca0476a10acaaec3483127339989edf
georgiybykov <zhora.bykov@gmail.com>
2019-12-01 01:49:59 +0300

    Lesson 2. Edited alphabet_hash.rb by adding new method .succ; Created file for 5th exercise

--- Lesson 2/alphabet_hash.rb
+++ Lesson 2/alphabet_hash.rb
@@ -7,7 +7,7 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 hash_vow = {}
 
 vowels.each do |v|
-  hash_vow[v] = alph_arr.find_index(v) + 1
+  hash_vow[v] = alph_arr.find_index(v).succ # метод .succ возвращает непосредственного наследника числа, то есть number + 1
 end
 
 puts hash_vow
---
+++ Lesson 2/date_serial_number.rb
@@ -0,0 +1,11 @@
+=begin
+
+=end
+
+puts "Type the day of the month: "
+day = gets.chomp
+puts "Type month: "
+month = = gets.chomp
+puts "Type year: "
+year = = gets.chomp
+
