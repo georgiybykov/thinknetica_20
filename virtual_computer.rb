a54eb52407c54edb08a293b7cc9ffeb5563e2b98
georgiybykov <zhora.bykov@gmail.com>
2019-12-14 03:45:42 +0300

    virtual_computer.rb: it was added delete method

--- virtual_computer.rb
+++ virtual_computer.rb
@@ -15,6 +15,12 @@ class Computer
     puts "A new file #{filename} was created at #{time} by #{@username}."
   end
 
+  def delete(filename)
+    time = Time.now
+    @files.delete(filename)
+    puts "File #{filename} was deleted at #{time} by #{@username}."
+  end
+
   def Computer.get_users
     @@users
   end
@@ -23,5 +29,7 @@ end
 my_computer = Computer.new('Mike', 123456)
 
 my_computer.create("text.txt")
+my_computer.create("example.txt")
+my_computer.delete("text.txt")
 
 puts "Users: #{Computer.get_users}"
