187f7b5a96aca31ee30cbbee1b6840b7e895dede
georgiybykov <zhora.bykov@gmail.com>
2020-01-09 04:28:16 +0300

    Edited comment in fibonacci file

--- Fibonacci through recursion/fibonacci_recursion.rb
+++ Fibonacci through recursion/fibonacci_recursion.rb
@@ -1,5 +1,11 @@
 =begin
-Each time the code branches off again you are calling the fibonacci function from within itself two times. If you take all of those ones and zeros and add them together, you'll get the same answer you get when you run the code. You can see why computer programs are handy now. Think if you had to draw that diagram out every time you wanted to know the fibonacci respresentation of a number.
+
+from launchschool.com
+
+Each time the code branches off again you are calling the fibonacci function from within itself two times.
+If you take all of those ones and zeros and add them together, you'll get the same answer you get when you run the code.
+You can see why computer programs are handy now.
+Think if you had to draw that diagram out every time you wanted to know the fibonacci respresentation of a number.
 =end
 
 
