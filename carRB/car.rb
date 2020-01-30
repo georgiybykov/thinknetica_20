1cf981af1344dd645f92178b0005ca2b356e06c0
georgiybykov <zhora.bykov@gmail.com>
2020-01-31 02:03:31 +0300

    Screencast Lesson 6

--- carRB/car.rb
+++ carRB/car.rb
@@ -2,8 +2,22 @@ class Car
 
   attr_reader :current_rpm
 
+  @@instances = 0
+
+  def self.instances
+    @@instances
+  end
+
+  def self.debug(log)
+    puts "!!!DEBUG: #{log} !!!"
+  end
+
+  debug 'Start interface'
+
   def initialize
     @current_rpm = 0
+    @@instances += 1
+    self.class.debug 'Initialize'
   end
 
   def start_engine
@@ -13,6 +27,8 @@ class Car
   def engine_stopped?
     current_rpm.zero?
   end
+
+  debug 'End interface'
 
   protected
 
---
+++ carRB/modules.rb
@@ -0,0 +1,10 @@
+# Пространство имен
+
+# module Admin
+#   class Car
+#     # реализация
+#   end
+# end
+
+class Admin::Car
+end
