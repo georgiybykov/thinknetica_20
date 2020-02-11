2da9e8a2d1178f4ac0b7e0c43f2fed886e518512
georgiybykov <zhora.bykov@gmail.com>
2020-02-11 21:41:19 +0300

    updated my_car file from launchschool tutorial; added module, some classes and inheritance between them

--- my_car_launchschoolOOP.rb
+++ my_car_launchschoolOOP.rb
@@ -1,25 +1,31 @@
-class MyCar
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000 ? true : false
+  end
+end
+
+
+class Vechicle
 
   attr_accessor :color, :speed
   attr_reader :year, :model
+
+  @@number_of_vechicles = 0
 
   def initialize(year, color, model)
     @year = year
     @color = color
     @model = model
     @speed = 0
+    @@number_of_vechicles += 1
   end
 
-  def self.what_i_am
-    puts 'I am a MyCar class!'
+  def self.number_of_vechicles
+    puts "This program has created #{@@number_of_vechicles} number of vechicles."
   end
 
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas."
-  end
-
-  def to_s
-    puts "Hello, mate! Glad to see you here! Your car is #{model}, the color is #{color} and the year is #{year}."
   end
 
   def start_engine
@@ -41,9 +47,48 @@ class MyCar
 
   def spray_paint(color)
     @color = color
-    puts "Your car is #{@color} color now."
+    puts "Your #{@model} vechicle is #{@color} color now."
+  end
+
+  def age
+    puts "Your #{@model} age is #{years_old} years old."
+  end
+
+  private
+
+  def years_old
+    Time.now.year - self.year
   end
 end
+
+class MyCar < Vechicle
+
+  NUMBER_OF_DOORS = 4
+
+  def self.what_i_am
+    puts 'I am a MyCar class!'
+  end
+
+  def to_s
+    puts "Hello, mate! Glad to see you here! Your car is #{model}, the color is #{color} and the year is #{year}."
+  end
+end
+
+class MyTruck < Vechicle
+
+  include Towable
+
+  NUMBER_OF_DOORS = 2
+
+  def self.what_i_am
+    puts 'I am a MyTruck class!'
+  end
+
+  def to_s
+    puts "Hello, mate! Glad to see you here! Your truck is #{model}, the color is #{color} and the year is #{year}."
+  end
+end
+
 
 car = MyCar.new(2008, 'black', 'BMW')
 puts car
@@ -60,3 +105,35 @@ car.spray_paint('white')
 puts car.color
 MyCar.what_i_am
 MyCar.gas_mileage(10, 524)
+
+truck = MyTruck.new(2010, 'blue', 'Mercedes-Benz')
+puts truck
+truck.start_engine
+truck.go(65)
+puts truck.speed
+puts truck.year
+puts truck.color
+puts truck.model
+truck.stop
+puts truck.speed
+truck.stop_engine
+truck.spray_paint('green')
+puts truck.color
+MyTruck.what_i_am
+MyTruck.gas_mileage(15, 865)
+
+Vechicle.number_of_vechicles
+
+puts truck.can_tow?(2530)
+puts truck.can_tow?(1550)
+
+puts '-----------------'
+puts Vechicle.ancestors
+puts '-----------------'
+puts MyCar.ancestors
+puts '-----------------'
+puts MyTruck.ancestors
+puts '-----------------'
+
+car.age
+truck.age
