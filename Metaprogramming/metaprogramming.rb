c4d22454cbb124ecfe4dbe4b73d7a17d2084f9e7
georgiybykov <zhora.bykov@gmail.com>
2020-03-11 16:52:35 +0300

    selfmade attr_accessor

--- Metaprogramming/metaprogramming.rb
+++ Metaprogramming/metaprogramming.rb
@@ -25,3 +25,17 @@ class Test
 
   my_attr_accessor :my_attr, :a, :b, :q, :someone
 end
+
+# class Module
+#   def attr_accessor(*methods)
+#     methods.each do |method|
+#       raise TypeError.new('method is not a symbol') unless method.is_a?(Symbol)
+#       define_method(method) do
+#         instance_variable_get("@#{method}")
+#       end
+#       define_method("@#{method}=") do |v|
+#         instance_variable_set("@#{method}", v)
+#       end
+#     end
+#   end
+# end
